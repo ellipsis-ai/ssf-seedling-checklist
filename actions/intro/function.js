39f2db4c6ea39c6ee53aed669b5777fa928122ab
Andrew Catton <>
2018-05-07 14:45:46 +0000

    Added an "All good" option to quick respond

--- actions/intro/function.js
+++ actions/intro/function.js
@@ -2,13 +2,20 @@ function(ellipsis) {
   const intro = `
 🌱Hello, this is the SSF Seedling Systems Checklist reminder. 
 
-Anyone in this channel can answer the following five yes/no questions. Click below to begin.
+Anyone in this channel can respond. Click \`Start\` to begin or \`All good\` to quickly report that everything is in order.
 `;
 ellipsis.success(intro, {
-  choices: [{
-    label: "Start",
-    actionName: "run-checklist",
-    allowOthers: true
-  }]
+  choices: [
+    {
+      label: "Start",
+      actionName: "run-checklist",
+      allowOthers: true
+    },
+    {
+      label: ":white_check_mark: All good",
+      actionName: "all-good",
+      allowOthers: true
+    }
+  ]
 });
 }
